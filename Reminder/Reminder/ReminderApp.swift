94d8e342c51d1aee03bbb3bc0df54a915921f6af
Sherry1481 <shareyscorpi1@gmail.com>
2021-04-08 00:22:16 +0500

    commit

---
+++ Reminder/Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by sherry on 07/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ReminderList()
+        }
+    }
+}
